ab0d7752ecc65cb712fc233acd3736400913c665
allanski123 <allanlai1998@hotmail.com>
2019-02-28 17:30:25 +1100

    Initial Commit

---
+++ mySalesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mySalesApp
+//
+//  Created by allanski on 28/2/19.
+//  Copyright © 2019 allanski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
